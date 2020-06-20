98bc0db15f0e53acb82b62ef1c7f2a5d42fb3ead
yitian6 <yitianSang11@gmail.com>
2020-06-20 22:25:12 +0800

    Site updated: 2020-06-20 22:25:09

--- js/cursor/text.js
+++ js/cursor/text.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 17 2020 17:27:35 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 20 2020 22:25:03 GMT+0800 (GMT+08:00)
 var a_idx=0;jQuery(document).ready(function(a){a("body").click(function(e){var n=new Array("喜欢我","不喜欢我");var o=a("<span/>").text(n[a_idx]);var t=e.pageX,i=e.pageY;o.css({"z-index":99999,top:i-28,left:t-n[a_idx].length*8,position:"absolute",color:"#ff7a45"});a("body").append(o);o.animate({top:i-180,opacity:0},1500,function(){o.remove()});a_idx=(a_idx+1)%n.length})});
 //rebuild by neat 
